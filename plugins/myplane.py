7a6ec83a2dbcc4a7d39979c1d9c8d3385f49ad6f
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-11-13 00:54:17 +0530

    Update myplane.py

--- plugins/myplane.py
+++ plugins/myplane.py
@@ -37,12 +37,12 @@ async def start(client, message):
             uploadlimit(message.from_user.id, 21474836480)
             usertype(message.from_user.id, "Free")
     if ends == None:
-        text = f"User ID:- `{message.from_user.id}`\nPlan :- {user}\nDaly Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}"
+        text = f"𝐔𝐬𝐞𝐫 𝐈𝐃 :- `{message.from_user.id}`\n𝐏𝐥𝐚𝐧 :- {user}\n𝐃𝐚𝐢𝐥𝐲 𝐋𝐢𝐦𝐢𝐭 :- {humanbytes(limit)}\n𝐓𝐨𝐝𝐚𝐲 𝐔𝐬𝐞𝐝 :- {humanbytes(used)}\n𝐑𝐞𝐦𝐚𝐢𝐧𝐢𝐧𝐠 :- {humanbytes(remain)}"
     else:
         normal_date = datetime.fromtimestamp(ends).strftime('%Y-%m-%d')
-        text = f"User ID:- `{message.from_user.id}`\nPlan :- {user}\nDaly Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}\n\nYour Plan Ends On :- {normal_date}"
+        text = f"𝐔𝐬𝐞𝐫 𝐈𝐃 :- `{message.from_user.id}`\n𝐏𝐥𝐚𝐧 :- {user}\n𝐃𝐚𝐢𝐥𝐲 𝐋𝐢𝐦𝐢𝐭 :- {humanbytes(limit)}\n𝐓𝐨𝐝𝐚𝐲 𝐔𝐬𝐞𝐝 :- {humanbytes(used)}\n𝐑𝐞𝐦𝐚𝐢𝐧𝐢𝐧𝐠 :- {humanbytes(remain)}\n\n𝐘𝐨𝐮𝐫 𝐏𝐥𝐚𝐧 𝐄𝐧𝐝𝐬 𝐎𝐧 :- {normal_date}"
 
     if user == "Free":
-        await message.reply(text, quote=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Upgrade 💰💳", callback_data="upgrade"), InlineKeyboardButton("Cancel ✖️ ", callback_data="cancel")]]))
+        await message.reply(text, quote=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘɢʀᴀᴅᴇ  ʏᴏᴜʀ  ᴘʟᴀɴ  💳", callback_data="upgrade"), InlineKeyboardButton("Cᴀɴᴄᴇʟ ✖️ ", callback_data="cancel")]]))
     else:
         await message.reply(text, quote=True)
