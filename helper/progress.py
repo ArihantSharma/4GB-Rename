05f9e5129850a4fab183f01710e8d0e3bd7e7313
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-11-18 19:54:33 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -70,11 +70,3 @@ def TimeFormatter(milliseconds: int) -> str:
         ((str(seconds) + "s, ") if seconds else "") + \
         ((str(milliseconds) + "ms, ") if milliseconds else "")
     return tmp[:-2] 
-
-def convert(seconds):
-    seconds = seconds % (24 * 3600)
-    hour = seconds // 3600
-    seconds %= 3600
-    minutes = seconds // 60
-    seconds %= 60      
-    return "%d:%02d:%02d" % (hour, minutes, seconds)
